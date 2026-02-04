7a5aad8c5e40ee6d5738b6ddab1cebd31286da04
Sushmitha-Chowdary <nsushmithachowdary2006@gmail.com>
2026-02-04 18:08:02 +0530

    practice

---
+++ greaterelement.py
@@ -0,0 +1,14 @@
+def nextGreaterElement(nums):
+    st=[]
+    n=len(nums)
+    ans=[-1]*n
+    for i in range(2*n):
+        idx=i%n
+        while st and nums[idx]>nums[st[-1]]:
+            prev=st.pop()
+            ans[prev]=nums[idx]
+        if i<n:
+            st.append(i)
+    return ans
+nums=list(map(int,input("Enter nums:").split()))
+print(nextGreaterElement(nums))
